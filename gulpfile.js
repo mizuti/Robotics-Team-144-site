f66654020bc6bfc6813d881b7b049a273a297b7a
mizuti <kyle.rentschler1@gmail.com>
2017-10-25 16:34:33 -0400

    Added CSS Framwork base, update build process for new framework

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
-var gulp = require('gulp');
-var handlebars = require('gulp-compile-handlebars');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
-var header = require('gulp-header');
-var cleanCSS = require('gulp-clean-css');
-var rename = require("gulp-rename");
-var uglify = require('gulp-uglify');
-var pkg = require('./package.json');
+'use strict';
+
+var browserSync = require('browser-sync').create(),
+    path = require('path'),
+    del = require('del'),
+    gulp = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    notify = require('gulp-notify'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    handlebars = require('gulp-compile-handlebars'),
+    header = require('gulp-header'),
+    rename = require("gulp-rename"),
+    uglify = require('gulp-uglify'),
+    runSequence = require('run-sequence');
+
 
 /************************************************************************************/
 // Compile handlebars templates
@@ -47,27 +54,43 @@ gulp.task('handlebars', function() {
 
 
 
-// Compile SCSS files from /scss into /css
-gulp.task('sass', function() {
-    return gulp.src('scss/main.scss')
-        .pipe(sass())
-        // .pipe(header(banner, { pkg: pkg }))
-        .pipe(gulp.dest('dist/css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
+// Task to compile SCSS
+gulp.task('sass', function () {
+    return gulp.src('./scss/main.scss')
+      .pipe(sourcemaps.init())
+      .pipe(sass({
+        outputStyle: 'nested',
+        errLogToConsole: false,
+        paths: [path.join(__dirname, 'scss', 'includes')]
+      })
+        .on("error", notify.onError(function (error) {
+          return "Failed to Compile SCSS: " + error.message;
+        })))
+      .pipe(autoprefixer())
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('dist/css/'))
+      .pipe(browserSync.stream())
+      .pipe(notify({
+        message: "SCSS Compiled Successfully :)",
+        onLast: true
+      }));
 });
 
-// Minify compiled CSS
-gulp.task('minify-css', ['sass'], function() {
-    return gulp.src('css/main.css')
-        .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('dist/css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
+// Gulp Clean Up Task
+gulp.task('clean', function () {
+  return del('dist/css/');
 });
+
+// // Minify compiled CSS
+// gulp.task('minify-css', ['sass'], function() {
+//     return gulp.src('css/main.css')
+//         .pipe(cleanCSS({ compatibility: 'ie8' }))
+//         .pipe(rename({ suffix: '.min' }))
+//         .pipe(gulp.dest('dist/css'))
+//         .pipe(browserSync.reload({
+//             stream: true
+//         }))
+// });
 
 
 
@@ -75,7 +98,6 @@ gulp.task('minify-css', ['sass'], function() {
 gulp.task('minify-js', function() {
     return gulp.src('js/main.js')
         .pipe(uglify())
-        // .pipe(header(banner, { pkg: pkg }))
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('dist/js'))
         .pipe(browserSync.reload({
@@ -85,17 +107,8 @@ gulp.task('minify-js', function() {
 
 // Copy vendor libraries from /node_modules into /vendor
 gulp.task('copy', function() {
-    gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('dist/vendor/bootstrap'))
-
     gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
         .pipe(gulp.dest('dist/vendor/jquery'))
-
-    gulp.src(['node_modules/magnific-popup/dist/*'])
-        .pipe(gulp.dest('dist/vendor/magnific-popup'))
-
-    gulp.src(['node_modules/scrollreveal/dist/*.js'])
-        .pipe(gulp.dest('dist/vendor/scrollreveal'))
 
     gulp.src([
             'node_modules/font-awesome/**',
@@ -106,11 +119,24 @@ gulp.task('copy', function() {
             '!node_modules/font-awesome/*.json'
         ])
         .pipe(gulp.dest('dist/vendor/font-awesome'))
+    
+    gulp.src([
+        '.htaccess',
+        'robots.txt',
+        'humans.txt',
+        'favicon.ico',
+        'icon.png',
+        'tile-wide.png',
+        'tile.png',
+        '404.html',
+        'browserconfig.xml'
+    ])
+    .pipe(gulp.dest('dist/'))
 })
 
 
 // Run everything
-gulp.task('default', ['sass', 'minify-css', 'minify-js', 'handlebars', 'copy']);
+gulp.task('default', ['clean', 'sass', 'minify-js', 'handlebars', 'copy']);
 
 // Configure the browserSync task
 gulp.task('browserSync', function() {
@@ -122,7 +148,7 @@ gulp.task('browserSync', function() {
 })
 
 // Dev task with browserSync
-gulp.task('dev', ['browserSync', 'sass', 'minify-css', 'handlebars', 'minify-js'], function() {
+gulp.task('dev', ['browserSync','clean', 'sass', 'handlebars', 'minify-js'], function() {
     gulp.watch('scss/*.scss', ['sass']);
     gulp.watch('css/*.css', ['minify-css']);
     gulp.watch('js/*.js', ['minify-js']);

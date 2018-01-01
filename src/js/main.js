dc74d6b61d6943908eb099073bc422233387473e
mizuti <kyle.rentschler1@gmail.com>
2018-01-01 18:06:20 -0500

    styled home header

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,30 @@
 jQuery(function($) {
     $(document).ready(function(){
-        var scrolling = false;
-        var navBar = $('.navbar');
+        // var scrolling = false;
+        // var navBar = $('.navbar');
 
-        function setScrolling() {
-            navBar.removeClass('scroll-top');
-            navBar.addClass('scrolling');
-        }
+        // function setScrolling() {
+        //     navBar.removeClass('scroll-top');
+        //     navBar.addClass('scrolling');
+        // }
 
-        function stopScrolling() {
-            navBar.removeClass('scrolling');
-            navBar.addClass('scrolling-top');
-        }
+        // function stopScrolling() {
+        //     navBar.removeClass('scrolling');
+        //     navBar.addClass('scrolling-top');
+        // }
 
-        $(window).scroll(function() { scrolling = true; });
+        // $(window).scroll(function() { scrolling = true; });
 
-        setInterval(function() {
-            if(scrolling) {
-                scrolling = false;
-                if ($(window).scrollTop() > 0) {
-                    setScrolling();
-                } else {
-                    stopScrolling();
-                }
-            }
-        }, 100);
+        // setInterval(function() {
+        //     if(scrolling) {
+        //         scrolling = false;
+        //         if ($(window).scrollTop() > 0) {
+        //             setScrolling();
+        //         } else {
+        //             stopScrolling();
+        //         }
+        //     }
+        // }, 100);
 
     });
  });
